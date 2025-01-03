96eba7ac24e20f85b3297391a3ddb089888c08de
Alston Dsouza <alston@Alstons-MacBook-Pro.local>
2025-01-03 13:35:25 -0800

    completed Number Practice and added things to refresh

--- src/NumberPractice.java
+++ src/NumberPractice.java
@@ -1,18 +1,31 @@
 public class NumberPractice {
   public static void main(String args[]) {
     // Create a float with a negative value and assign it to a variable
+    float negativeFloat = -5.67f;
+    System.out.println("Negative float: " + negativeFloat);
 
     // Create an int with a positive value and assign it to a variable
+    int positiveInt = 42;
+    System.out.println("Positive int: " + positiveInt);
 
     // Use the modulo % operator to find the remainder when the int is divided by 3
+    int remainderBy3 = positiveInt % 3;
+    System.out.println("Remainder when divided by 3: " + remainderBy3);
 
     // Use the modulo % operator to determine whether the number is even
     // (A number is even if it has a remainder of zero when divided by 2)
     // Use an if-else to print "Even" if the number is even and "Odd"
     // if the number is odd.
+    if (positiveInt % 2 == 0) {
+      System.out.println("Even");
+    } else {
+      System.out.println("Odd");
+    }
 
     // Divide the number by another number using integer division
-
+    int divisionResult = positiveInt / 5;
+    System.out.println("Result of integer division: " + divisionResult);
+    
     /*
      * Reminder!
      * 
--- toRefresh.md
+++ toRefresh.md
@@ -2,4 +2,11 @@
 
 As you work through this exercise, write down anything that you needed to look up or struggled to remember here. It can be just a word or two (e.g. "joining strings"). You can use this as a guide of what to make extra sure you're refreshed on before exams and interviews.
 
-- 
+## NumberPractice 
+
+- Float syntax (using f suffix)
+- Modulo operator % for even/odd checks
+- Integer division always rounds down
+- Printing things with System.out.println
+
+
